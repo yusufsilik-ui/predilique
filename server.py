1bd1f9a8ae3c85653853d6924386777c79751885
yusufsilik-ui <yusufsilik@gmail.com>
2025-09-02 18:39:51 +0300

    Update server.py
    binance API key with default added

--- server.py
+++ server.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
+from binance.client import Client
+import os
 
 app = Flask(__name__)
 
+# Load Binance API keys from environment variables
+API_KEY = os.getenv("BINANCE_API_KEY")
+API_SECRET = os.getenv("BINANCE_API_SECRET")
+
+# Initialize Binance client
+client = Client(API_KEY, API_SECRET)
+
 @app.route("/")
 def index():
-    return render_template("index.html", title="Predilique")
+    # Get current BTCUSDT price
+    btc_price = client.get_symbol_ticker(symbol="BTCUSDT")["price"]
+    return render_template("index.html", title="Predilique", btc_price=btc_price)
 
 if __name__ == "__main__":
-    import os
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
